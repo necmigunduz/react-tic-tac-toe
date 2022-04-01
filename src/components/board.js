92caf6aa76026d84e28a6e2ecd9313b039c37917
necmigunduz <necmigunduz@gmail.com>
2022-04-01 17:10:08 +0300

    Add board class component

--- src/components/board.js
+++ src/components/board.js
@@ -1,10 +1,17 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
-import PrototypeSymbol from './symbol';
-import SymbolX from './x';
-import SymbolO from './o';
-import Result from './result';
-import { X, O } from '../redux/reducer';
-import { placeSymbol, resetGame } from '../redux/actions';
+// Imports
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import PrototypeSymbol from "./symbol";
+import SymbolX from "./x";
+import SymbolO from "./o";
+import Result from "./result";
+import { X, O } from "../redux/reducer";
+import { placeSymbol, resetGame } from "../redux/actions";
 
+// Board class component
+class Board extends Component {
+  render() {
+    return <div>board</div>;
+  }
+}
