745eb52d634066c7ab07ce92cf9ccec2eb1d943c
necmigunduz <necmigunduz@gmail.com>
2022-04-01 17:01:02 +0300

    Return result of winner and winning line

--- src/components/result.js
+++ src/components/result.js
@@ -14,7 +14,8 @@ class Result extends Component {
     // Define if someone wins the gam
     if (this.props.winning) {
       result = `Game result: ${this.props.winning.toUpperCase()} wins the game.`;
-    } else if (this.props.draw) { // Define if the game is ended with a draw
+    } else if (this.props.draw) {
+      // Define if the game is ended with a draw
       result = (
         <p>
           Game result: It is a draw! To replay the game, click on Restart the
@@ -23,24 +24,38 @@ class Result extends Component {
       );
     }
     // Define winning line
-    let winL ='';
-    if(this.props.winningLine==='horizontal0'){ // first row is winning
-      winL = 'Winning row: First row'
-    } else if(this.props.winningLine==='horizontal1'){ // second row is winning
-      winL = 'Winning row: Second row'
-    } else if(this.props.winningLine==='horizontal2'){ // third row is winning
-      winL = 'Winning row: Third row'
-    } else if(this.props.winningLine==='vertical0'){ // first column is winning
-      winL = 'Winning column: First column'
-    } else if(this.props.winningLine==='vertical1'){ // second column is winning
-      winL = 'Winning column: Second column'
-    } else if(this.props.winningLine==='vertical2'){ // third column is winning
-      winL = 'Winning column: Third column'
-    } else if(this.props.winningLine==='crossLeft'){ // left to right cross is winning
-      winL = 'Winning cross cells: Left to Right'
-    } else if(this.props.winningLine==='crossRight'){ // right to left cross is winning
-      winL = 'Winning cross cells: Right to Left'
+    let winL = "";
+    if (this.props.winningLine === "horizontal0") {
+      // first row is winning
+      winL = "Winning row: First row";
+    } else if (this.props.winningLine === "horizontal1") {
+      // second row is winning
+      winL = "Winning row: Second row";
+    } else if (this.props.winningLine === "horizontal2") {
+      // third row is winning
+      winL = "Winning row: Third row";
+    } else if (this.props.winningLine === "vertical0") {
+      // first column is winning
+      winL = "Winning column: First column";
+    } else if (this.props.winningLine === "vertical1") {
+      // second column is winning
+      winL = "Winning column: Second column";
+    } else if (this.props.winningLine === "vertical2") {
+      // third column is winning
+      winL = "Winning column: Third column";
+    } else if (this.props.winningLine === "crossLeft") {
+      // left to right cross is winning
+      winL = "Winning cross cells: Left to Right";
+    } else if (this.props.winningLine === "crossRight") {
+      // right to left cross is winning
+      winL = "Winning cross cells: Right to Left";
     }
-    return <div>result</div>;
+    return (
+      // Return result and winning line 
+      <div>
+        <h3>{result}</h3>
+        <p>{winL}</p>
+      </div>
+    );
   }
 }
