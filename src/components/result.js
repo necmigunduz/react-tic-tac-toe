81975c6a5610e8c4392eaaf2ba0e43900745dac8
necmigunduz <necmigunduz@gmail.com>
2022-04-01 16:59:28 +0300

    Define winning line

--- src/components/result.js
+++ src/components/result.js
@@ -22,6 +22,25 @@ class Result extends Component {
         </p>
       );
     }
+    // Define winning line
+    let winL ='';
+    if(this.props.winningLine==='horizontal0'){ // first row is winning
+      winL = 'Winning row: First row'
+    } else if(this.props.winningLine==='horizontal1'){ // second row is winning
+      winL = 'Winning row: Second row'
+    } else if(this.props.winningLine==='horizontal2'){ // third row is winning
+      winL = 'Winning row: Third row'
+    } else if(this.props.winningLine==='vertical0'){ // first column is winning
+      winL = 'Winning column: First column'
+    } else if(this.props.winningLine==='vertical1'){ // second column is winning
+      winL = 'Winning column: Second column'
+    } else if(this.props.winningLine==='vertical2'){ // third column is winning
+      winL = 'Winning column: Third column'
+    } else if(this.props.winningLine==='crossLeft'){ // left to right cross is winning
+      winL = 'Winning cross cells: Left to Right'
+    } else if(this.props.winningLine==='crossRight'){ // right to left cross is winning
+      winL = 'Winning cross cells: Right to Left'
+    }
     return <div>result</div>;
   }
 }
