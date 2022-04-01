bee98ccfc6afd62130f78df4f7ea2e83364f7905
necmigunduz <necmigunduz@gmail.com>
2022-04-01 16:51:04 +0300

    Add conditions for turn, winning and draw and return a message

--- src/components/result.js
+++ src/components/result.js
@@ -1,12 +1,27 @@
 // Imports
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
 
-export default class Result extends Component {
+class Result extends Component {
   render() {
-    return (
-      <div>result</div>
-    )
+    // Creating a conditional structure for result
+    let result = "";
+    // Define turn and add the message
+    if (this.props.turn) {
+      result = `Turn: It is ${this.props.turn.toUpperCase()}'s turn.`;
+    }
+    // Define if someone wins the gam
+    if (this.props.winning) {
+      result = `Game result: ${this.props.winning.toUpperCase()} wins the game.`;
+    } else if (this.props.draw) { // Define if the game is ended with a draw
+      result = (
+        <p>
+          Game result: It is a draw! To replay the game, click on Restart the
+          game button.
+        </p>
+      );
+    }
+    return <div>result</div>;
   }
 }
