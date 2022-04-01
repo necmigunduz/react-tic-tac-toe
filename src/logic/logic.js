d066ff6ff77a3de4a70cf37c3f1a0e4ea1e2f89e
necmigunduz <necmigunduz@gmail.com>
2022-04-01 15:12:32 +0300

    Add logic for the winner in columns

--- src/logic/logic.js
+++ src/logic/logic.js
@@ -3,3 +3,15 @@ const countRow = (symbol, horizontal) =>
   horizontal.filter((e) => e === symbol).length;
 const wonRow = (symbol, horizontal) => countRow(symbol, horizontal) === 3;
 const threatRow = (symbol, horizontal) => countRow(symbol, horizontal) === 2;
+
+// Counting a symbol in a column and deciding whether game is won or there exists a threat
+const countCol = (symbol, vertical, ...horizontals) =>
+  horizontals
+    .map((horizontal) => horizontal[vertical])
+    .filter((e) => e === symbol).length;
+const wonCol = (symbol, vertical, ...horizontals) =>
+  countCol(symbol, vertical, ...horizontals) === 3;
+const threatCol = (symbol, vertical, ...horizontals) =>
+  countCol(symbol, vertical, ...horizontals) === 2;
+
+  
