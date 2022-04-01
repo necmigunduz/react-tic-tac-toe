c2232c7724b9bfb2b9e173a3cdef331a8cf278d7
necmigunduz <necmigunduz@gmail.com>
2022-04-01 17:35:17 +0300

    Add App component and its imports

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+// Imports
+import React from 'react';
+import styled from 'styled-components';
+import Board from './components/board';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
     <div>
-      Tic-Tac-Toe Game
+      <h1>Two-Player Tic-Tac-Toe Game</h1>
+      <Board className='Board' />
     </div>
   );
 }
