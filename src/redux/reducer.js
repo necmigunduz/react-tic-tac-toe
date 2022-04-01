d75e61c22d3a3863081eb7c7cabea40d6faf73bc
necmigunduz <necmigunduz@gmail.com>
2022-04-01 15:55:10 +0300

    Add conditions to understand the winning symbol and the winning line by using the result method from logic

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -21,7 +21,6 @@ const initialState = {
 const reducer = (state, action) => {
   switch (action.type) {
     case "PLACE_SYMBOL":
-      // Destruction of action
       const { symbol, horizontal, vertical } = action;
       // Create a copy the original state
       const newState = _.cloneDeep(state);
@@ -29,6 +28,22 @@ const reducer = (state, action) => {
       // Results for symbols of X and O
       const resultX = result(X, newState.board);
       const resultO = result(O, newState.board);
+
+      // Conditions to realize the winning symbol and the winning line or cross
+      // If X is the winner
+      if (resultX.winning) {
+        newState.winning = X;
+        newState.winningLine = resultX.line;
+      }
+      // If O is the winner
+      if (resultO.winning) {
+        newState.winning = O;
+        newState.winningLine = resultO.line;
+      }
+      // If no one wins, decision who will make the next turn
+      if (!newState.winning) {
+        newState.turn = newState.turn === O ? X : O;
+      }
 
       return newState;
     case "RESET_GAME":
