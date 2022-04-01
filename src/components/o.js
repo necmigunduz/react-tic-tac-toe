77198fef499a1db0acf96431c0533af5e87d8200
necmigunduz <necmigunduz@gmail.com>
2022-04-01 16:40:05 +0300

    Add symbolO

--- src/components/o.js
+++ src/components/o.js
@@ -1,3 +1,19 @@
+// Imports
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Symbol } from './symbol';
+
+// Create symbol O 
+// Symbol O Component
+const SymbolO = () => {
+    return(
+        <Symbol>
+            {/* Create O symbol with a circle placed in a svg tag */}
+            <svg viewBox='0 0 60 60'>
+                <circle cx={30} cy={30} r={25} stroke='white' strokeWidth='10' fill='black' />
+            </svg>
+        </Symbol> 
+    )
+};
+
+export default SymbolO;
