0ee03923c82bd60c9fa82b01fe5ff4385d38a69b
necmigunduz <necmigunduz@gmail.com>
2022-04-01 17:45:13 +0300

    Add class to symbolX and define type

--- src/components/x.js
+++ src/components/x.js
@@ -4,9 +4,9 @@ import PropTypes from "prop-types";
 import { Symbol } from "./symbol";
 
 // Create symbol X (Symbol X Component)
-const SymbolX = () => {
+const SymbolX = (props) => {
   return (
-    <Symbol>
+    <Symbol className={`symbol column${props.vertical}`}>
       {/* Create X symbol with two lines crossing each other placed in a svg tag */}
       <svg viewBox="0 0 60 60">
         <line x1="3" y1="3" x2="60" y2="60" stroke="white" strokeWidth="10" />
@@ -15,5 +15,9 @@ const SymbolX = () => {
     </Symbol>
   );
 };
-
+// Define type
+SymbolX.protoTypes = {
+  position: PropTypes.number.isRequired,
+};
+// Export
 export default SymbolX;
