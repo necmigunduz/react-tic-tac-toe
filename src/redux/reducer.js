ddab5642d6a987adea3def29d1a5e58f9b87c502
necmigunduz <necmigunduz@gmail.com>
2022-04-01 15:47:40 +0300

    Create a copy of original state by using lodash and results for X and O symbols

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,30 +1,39 @@
-import * as _ from 'lodash';
-import { result } from '../logic/logic';
+import * as _ from "lodash";
+import { result } from "../logic/logic";
 
 // Add symbols
-const X = 'x';
-const O = 'o';
+const X = "x";
+const O = "o";
 
 // Add inital state
 const initialState = {
   board: {
-    0: ['', '', ''],
-    1: ['', '', ''],
-    2: ['', '', ''],
+    0: ["", "", ""],
+    1: ["", "", ""],
+    2: ["", "", ""],
   },
   winning: undefined,
   winningLine: undefined,
   draw: false,
-  turn: O
+  turn: O,
 };
 
 const reducer = (state, action) => {
-    switch(action.type) {
-        case 'PLACE_SYMBOL':
-            return newState;
-        case 'RESET_GAME':
-            return initialState;
-        default:
-            return state;        
-    };
+  switch (action.type) {
+    case "PLACE_SYMBOL":
+      // Destruction of action
+      const { symbol, horizontal, vertical } = action;
+      // Create a copy the original state
+      const newState = _.cloneDeep(state);
+      newState.board[horizontal][vertical] = symbol;
+      // Results for symbols of X and O
+      const resultX = result(X, newState.board);
+      const resultO = result(O, newState.board);
+
+      return newState;
+    case "RESET_GAME":
+      return initialState;
+    default:
+      return state;
+  }
 };
