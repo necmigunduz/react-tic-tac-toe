9d14a51e3b77e0d27d563f8f82a165f9e33faff9
necmigunduz <necmigunduz@gmail.com>
2022-04-01 18:16:50 +0300

    Fix the error of root in Index.js due to the change of React version from 17 to 18

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import { createStore } from "redux";
 import { reducer, initialState } from "./redux/reducer";
 import "./index.css";
 import App from "./App";
 
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
 const store = createStore(reducer, initialState);
 
-ReactDOM.render(
+root.render(
   <Provider store={store}>
     <App />
-  </Provider>,
-  document.getElementById("root")
+  </Provider>
 );
