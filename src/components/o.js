e0755e7cdbc6eaecb27585146d59d8d277cecc56
necmigunduz <necmigunduz@gmail.com>
2022-04-01 17:44:14 +0300

    Add class to symbolO and define type

--- src/components/o.js
+++ src/components/o.js
@@ -4,9 +4,9 @@ import PropTypes from 'prop-types';
 import { Symbol } from './symbol';
 
 // Create symbol O (Symbol O Component)
-const SymbolO = () => {
+const SymbolO = (props) => {
     return(
-        <Symbol>
+        <Symbol className={`symbol vertical${props.vertical}`}>
             {/* Create O symbol with a circle placed in a svg tag */}
             <svg viewBox='0 0 60 60'>
                 <circle cx={30} cy={30} r={25} stroke='white' strokeWidth='10' fill='black' />
@@ -14,5 +14,9 @@ const SymbolO = () => {
         </Symbol> 
     )
 };
-
+// Define type of vertical
+SymbolO.protoTypes = {
+    vertical: PropTypes.number.isRequired
+};
+// Export
 export default SymbolO;
