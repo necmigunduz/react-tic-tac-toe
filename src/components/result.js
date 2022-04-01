f9159a1f0724bfba7be90f2dc842c7217af87b2b
necmigunduz <necmigunduz@gmail.com>
2022-04-01 16:44:08 +0300

    Add class component structure

--- src/components/result.js
+++ src/components/result.js
@@ -3,3 +3,10 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
+export default class result extends Component {
+  render() {
+    return (
+      <div>result</div>
+    )
+  }
+}
